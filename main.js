0cecb99bc4b5b5a06d2a25f912c72750d8e74ddf
robertlanchsweerdt <signaleightdesign@gmail.com>
2021-05-16 18:10:16 -0400

    updated JS so computation logic is operational

--- main.js
+++ main.js
@@ -1,42 +1,47 @@
+// VARIABLES
 const userInput = document.getElementById('user-input');
 const operator = document.querySelectorAll('.operator');
 const btn = document.getElementById('submit');
 const output = document.getElementById('output');
 let userOperator;
-let totalValue = 0;
+let totalValue;
 
+// EVENT LISTENER FOR EACH OPERATOR
 operator.forEach((op) => {
   op.addEventListener('click', () => {
     userOperator = op.textContent;
-    totalValue = parseInt(userInput.value);
+    if (!totalValue) {
+      totalValue = parseInt(userInput.value);
+    }
     display();
   });
 });
 
+// EVENT LISTENER FOR SUBMIT
 btn.addEventListener('click', calculate);
 
+// CALCULATION FUNCTION
 function calculate() {
-  if (!userInput.value) return;
-
-  console.log(typeof userOperator, userOperator);
-
   switch (userOperator) {
     case '+':
       totalValue = totalValue + parseInt(userInput.value);
       break;
     case '-':
       totalValue = totalValue - parseInt(userInput.value);
+      break;
     case '*':
       totalValue = totalValue * parseInt(userInput.value);
+      break;
     case '/':
       totalValue = totalValue / parseInt(userInput.value);
+      break;
   }
 
   display();
 }
 
+// DISPLAY FUNCTION
 function display() {
   userInput.value = '';
-  console.log(totalValue);
   output.textContent = totalValue;
 }
